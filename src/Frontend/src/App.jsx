42a19d75520ffdb73860932ebc62e1681f15c1de
Rony George <ronygeorgengit@gmail.com>
2025-02-07 00:46:43 +0530

    navbar 4

--- src/Frontend/src/App.jsx
+++ src/Frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Toaster } from 'sonner';
 import { Routes, Route,useLocation, useNavigate, Navigate } from 'react-router-dom'
