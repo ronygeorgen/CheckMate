c124413145778723315bd4730637c0dc75f99fc6
Rony George <ronygeorgengit@gmail.com>
2025-02-07 10:31:33 +0530

    cors error

--- src/Backend/Backend/Backend/settings.py
+++ src/Backend/Backend/Backend/settings.py
@@ -111,6 +111,8 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
@@ -134,8 +136,7 @@ CORS_ALLOW_HEADERS = [
     "x-csrftoken",
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
 
 
